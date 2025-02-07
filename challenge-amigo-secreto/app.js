00d7484d0e9fd0b32ab74047d9cbc24e8d1af6b6
Angie <angiesamper18@gmail.com>
2025-02-06 23:48:18 -0300

    función actualizar lista de amigos

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -4,16 +4,35 @@ function agregarAmigo(){
 
     let nombre = document.querySelector("#amigo").value ;
     if(nombre === ''){
+
         alert('Por favor, inserte un nombre.');
-        limpiarInput();
+        limpiar();
         return;
-    } else{    amigos.push(nombre);
-    console.log('Array', amigos);
-    limpiarInput();
-    return;
+
+    } else{
+    
+        amigos.push(nombre);
+        limpiar();
+
+        listaDeAmigos();
+        return;
     }
 }
 
-function limpiarInput(){
+function limpiar(){
     document.querySelector("#amigo").value = '';
+    document.querySelector("#listaAmigos").innerHTML = '';
+}
+
+
+function listaDeAmigos(){
+    let lista = document.querySelector("#listaAmigos");
+    
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     }
+
+    return lista;
+}
